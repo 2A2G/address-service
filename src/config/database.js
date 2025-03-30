3960fd8ac934404097a47774829d610757a06715
carlos <carlosdelarosa1009@gmail.com>
2025-03-30 17:57:26 -0500

    Dockerizacion del proyecto, configuracion de la bd  y definicion del modelo

--- src/config/database.js
+++ src/config/database.js
@@ -12,15 +12,4 @@ const sequelize = new Sequelize(
     }
 );
 
-const testConnection = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Conexión a la base de datos establecida correctamente.');
-    } catch (error) {
-        console.error('No se pudo conectar a la base de datos:', error);
-    }
-};
-
-testConnection();
-
 module.exports = sequelize;
